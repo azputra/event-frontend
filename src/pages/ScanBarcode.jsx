1c591478ae896651879281a05ee0114faa090ea1
azputra <ahmadzp102@gmail.com>
2025-05-07 13:22:46 +0700

    update font scan barcode

--- src/pages/ScanBarcode.jsx
+++ src/pages/ScanBarcode.jsx
@@ -513,7 +513,7 @@ const ScanBarcode = () => {
                     <svg className="animate-pulse w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                     </svg>
-                    <p className="font-medium">Arahkan kamera ke barcode customer</p>
+                    <p className="text-xs">Arahkan kamera ke barcode customer</p>
                   </div>
                 )}
               </div>
